0bfb09c8caa95ad853e68dee229c45e80e36e020
mike hakiz <66285507+mikeycantcode@users.noreply.github.com>
2023-04-08 00:34:38 -0400

    did some stuff

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,6 +1,24 @@
 import React from "react";
 import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 import { auth } from "../firebase";
+import styled from 'styled-components';
+
+
+const SignInButton = styled.button`
+  background-color: #306bcf;
+  border: 1px solid #205bbf;
+  border-radius: 6px;
+  color: #fff;
+  cursor: pointer;
+  font-size: 16px;
+  font-weight: bold;
+  padding: 12px 24px;
+  transition: background-color 0.3s ease;
+
+  &:hover {
+    background-color: #111;
+  }
+`;
 
 const SignIn = () => {
     const signInWithGoogle = async () => {
@@ -14,9 +32,12 @@ const SignIn = () => {
 
     return (
         <div>
-            <button onClick={signInWithGoogle}>Sign in with Google</button>
+            <SignInButton onClick={signInWithGoogle}>
+                Sign in with Google
+            </SignInButton>
         </div>
     );
 };
 
 export default SignIn;
+
