5e17a49e933f0669438c19c2a29a9c5a704bbda2
mike hakiz <66285507+mikeycantcode@users.noreply.github.com>
2023-06-17 16:30:22 -0400

    MASSIVE SECUIRTY ISSUE LMFAO

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyC3VHOAUnguAcGRt_-GSrPVFU526q7wznI",
-    authDomain: "chatpdfapp.firebaseapp.com",
-    projectId: "chatpdfapp",
-    storageBucket: "chatpdfapp.appspot.com",
-    messagingSenderId: "613503853683",
-    appId: "1:613503853683:web:d12170aa6bbe8dd4d57606",
-    measurementId: "G-XMVHET41ZH"
+
 };
 
 const app = initializeApp(firebaseConfig);
